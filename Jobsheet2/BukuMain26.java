c91f29cab61a842c76da20a275cd86d4438b2d67
5atriy0 <satriyobagus47@gmail.com>
2024-02-19 10:58:40 +0700

    done

--- Jobsheet2/BukuMain26.java
+++ Jobsheet2/BukuMain26.java
@@ -17,5 +17,9 @@ public class BukuMain26 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+
+        Buku26 bk2 = new Buku26("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
     }
 }
